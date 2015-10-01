fd8442cb77164b9c5d3148c2c7d503443092cb11
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-10-01 14:45:47 -0400

    added a user_id column

---
+++ db/migrate/20151001183704_add_user_id_to_statuses.rb
@@ -0,0 +1,8 @@
+class AddUserIdToStatuses < ActiveRecord::Migration
+  def change
+    add_column :statuses, :user_id, :integer
+    #find the user based on the status
+    add_index :statuses, :user_id
+    remove_column :statuses, :name
+  end
+end
