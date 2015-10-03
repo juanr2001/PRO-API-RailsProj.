af3ef945707a39607dd246e96b57695a84415aee
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-10-03 13:56:18 -0400

    Made a controller for devise for stron params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
 
-    # before_action :sign_up_params || :account_update_params, if: :devise_controller?
-
     private
 
     def sign_up_params
@@ -13,9 +11,3 @@ class RegistrationsController < Devise::RegistrationsController
     end
 
 end
-        # devise_parameter_sanitizer.for(:sign_up) do | u |
-        #     u.permit( :first_name, :last_name, :profile_name, :email, :password, :password_confirmation)
-        # end
-        # devise_parameter_sanitizer.for(:sign_in) do | u |
-        #     u.permit( :profile_name, :password, :remember_me)
-        # end
