fa715239209ad8f65124eafc6a67998d6b7859ea
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-10-04 11:41:21 -0400

    Added Test and all passed after adding validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,47 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+    test "a user should enter a first name" do
+        user = User.new
+        assert !user.save
+        #passing error object to first name is not empty, this will fail but, but will pass if there is a validation in the models.
+        assert !user.errors[ :first_name ].empty?
+    end
+
+    test "a user should enter a last name" do
+        user = User.new
+        assert !user.save
+        #passing error object to first name is not empty, this will fail but, but will pass if there is a validation in the models.
+        assert !user.errors[ :last_name ].empty?
+    end
+
+    test "a user should enter a profile name" do
+        user = User.new
+        assert !user.save
+        #passing error object to first name is not empty, this will fail but, but will pass if there is a validation in the models.
+        assert !user.errors[ :profile_name ].empty?
+    end
+
+    test "a user should have a unique profile name" do
+        user = User.new
+        #calling the fixture/users.yml (:juan) in the parenthesis and call its profile_name. I used users, becuase that is the name of the file.
+        user.profile_name = users(:juan).profile_name
+        #If I put the password in the fixtures the test will fail
+        user.password = "password"
+        user.password_confirmation = "password"
+        assert !user.save
+        assert !user.errors[ :profile_name ].empty?
+    end
+
+    test "a user should have a profile name without spaces" do
+        user = User.new
+        user.profile_name = "My Profile with Spaces"
+
+        assert !user.save
+        #make sure there are some errors in the profile name
+        assert !user.errors[ :profile_name ].empty?
+        #make sure there is a correct errors message
+        assert user.errors[ :profile_name ].include?( "Must be formatted correctly." )
+    end
+
 end
