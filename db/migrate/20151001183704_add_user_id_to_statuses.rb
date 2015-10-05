eda6c9185d4812f63abfc0036ce1743a2677f37b
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-10-05 19:10:34 -0400

    fixing migrations

--- db/migrate/20151001183704_add_user_id_to_statuses.rb
+++ db/migrate/20151001183704_add_user_id_to_statuses.rb
@@ -2,7 +2,7 @@ class AddUserIdToStatuses < ActiveRecord::Migration
   def change
     add_column :statuses, :user_id, :integer
     #find the user based on the status
-    add_index :statuses, :user_id
+    # add_index :statuses, :user_id
     remove_column :statuses, :name
   end
 end
