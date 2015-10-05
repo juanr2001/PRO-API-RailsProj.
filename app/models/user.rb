88c4794ca4ff7352ccd46fe61c1954fa835337c4
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-10-05 12:46:22 -0400

    changed the formated expression

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
                                             uniqueness: true,
                                             format: {
 
-                                                            with: /a-zA-Z0-9_-/,
+                                                            with: /\A[a-zA-Z0-9_-]+\z/,
                                                             message: "Must be formatted correctly."
 
                                                         }
