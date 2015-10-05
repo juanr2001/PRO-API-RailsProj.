32e0046f56e266da37933a16035410f3c792ac9d
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-10-05 17:08:52 -0400

    There waas a bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,7 @@ class ApplicationController < ActionController::Base
         devise_parameter_sanitizer.for(:sign_up) { |u| u.permit( :first_name, :last_name, :profile_name, :email, :password, :password_confirm) }
         # devise_parameter_sanitizer.for(:account_update) << [:first_name, :last_name]
     end
-
+    def current
+        @current_user = current_user.first_name
+    end
 end
