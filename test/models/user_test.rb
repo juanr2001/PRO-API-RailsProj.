6b7fcf0d2e04fd17455e54ca0b5eed42cf38931e
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-10-06 15:59:18 -0400

    Added test to test friendship with users and models

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,6 +4,10 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+    should have_many( :user_friendships )
+    should have_many( :friends )
+
     test "a user should enter a first name" do
         user = User.new
         assert !user.save
@@ -55,7 +59,18 @@ class UserTest < ActiveSupport::TestCase
 
         user.profile_name = "Juancho"
         assert user.valid?
+    end
 
+    test " that no error is raised when trying to access a friend list" do
+        assert_nothing_raised do
+            users(:juan).friends
+        end
+    end
+
+    test "that creating friendships on a user works" do
+        users( :juan ).friends << users( :caramelo )
+        users( :juan ).friends.reload
+        assert users( :juan ).friends.include?( users( :caramelo ) )
     end
 
 end
